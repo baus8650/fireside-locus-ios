bca762d5153cdc49a0e73936b1ff4fd483e182e0
Tim Bausch <baus8650@gmail.com>
2022-04-07 11:22:35 -0700

    Initial Commit

---
+++ Fireside Locus/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Fireside Locus
+//
+//  Created by Tim Bausch on 4/7/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
